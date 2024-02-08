107a808254be8ba19c443ebd8db93e5d22903644
X-AROK <an_xarok@mail.ru>
2024-02-08 11:56:55 +0300

    Added env params

--- internal/app/config/flags.go
+++ internal/app/config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Addr string
 var BaseURL string
@@ -10,4 +13,11 @@ func ParseFlags() {
 	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
 
 	flag.Parse()
+
+	if envAddr := os.Getenv("SERVER_ADDRESS"); envAddr != "" {
+		Addr = envAddr
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		BaseURL = envBaseURL
+	}
 }
