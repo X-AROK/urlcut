a79c2119585f735feb4175b1aa5f3309c96fc31d
X-AROK <an_xarok@mail.ru>
2024-02-08 11:36:17 +0300

    Changed default base url

--- internal/app/config/flags.go
+++ internal/app/config/flags.go
@@ -7,7 +7,7 @@ var BaseURL string
 
 func ParseFlags() {
 	flag.StringVar(&Addr, "a", ":8080", "Адрес запуска HTTP-сервера")
-	flag.StringVar(&BaseURL, "b", "http://localhost:8000", "Базовый адрес результирующего сокращённого URL")
+	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
 
 	flag.Parse()
 }
