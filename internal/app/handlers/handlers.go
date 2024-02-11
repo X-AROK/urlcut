1ea52e596ba36841a8536a9bcd2558233e21cd41
X-AROK <an_xarok@mail.ru>
2024-02-11 11:33:52 +0300

    Added Recoverer middleware

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/X-AROK/urlcut/internal/app/store"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func MainRouter(s store.Repository, baseURL string) chi.Router {
 	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", createShort(s, baseURL))
 		r.Get("/{id}", goToID(s))
@@ -21,6 +23,7 @@ func MainRouter(s store.Repository, baseURL string) chi.Router {
 
 func createShort(s store.Repository, baseURL string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
 		buff, err := io.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, "Error in data", http.StatusBadRequest)
